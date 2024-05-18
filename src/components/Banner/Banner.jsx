008be96983d58b04c5636275e0a1c9503e1ed0ab
Kazi Golam Morshed <120186837+Morshed-17@users.noreply.github.com>
2024-05-19 00:40:58 +0600

    Update Banner.jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -16,7 +16,7 @@ import Slides from './Slides';
 const Banner = ({events}) => {
   return (
     <div style={{
-      backgroundImage: "url('https://demo.themewinter.com/wp/eventplace/wp-content/uploads/2022/11/hero_area_image_3.jpg')"
+      backgroundImage: "url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fabstract&psig=AOvVaw18grSyIOmztsz7HTEh1gWG&ust=1716144026236000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDtkeTsl4YDFQAAAAAdAAAAABAE')"
   }} className='bg-cover mt-[-110px]'>
       <Swiper 
     pagination={{
